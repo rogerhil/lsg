b19786854be9acf628c56d06ba70fc1bd2caede1
Rogerio Hilbert Lima <rogerio.hilbert.lima@ericsson.com>
2016-11-09 17:54:51 +0000

    User s feedback

--- web/lsg/client/master/js/custom/users/users.directives.js
+++ web/lsg/client/master/js/custom/users/users.directives.js
@@ -4,7 +4,9 @@
     angular
         .module('app.users')
         .directive('userRatings', userRatings)
-        .directive('userPhotoRatingsDistance', userPhotoRatingsDistance);
+        .directive('userPhotoRatingsDistance', userPhotoRatingsDistance)
+        .directive('userLatestFeedbacks', userLatestFeedbacks)
+        .controller('UserFeedbacksCtrl', UserFeedbacksCtrl)
 
     /*
      userRatings
@@ -21,19 +23,71 @@
     }
 
     /*
+     UserFeedbacksCtrl
+     */
+    UserFeedbacksCtrl.$inject = ['$mdDialog', 'UsersService', 'user', 'distance', 'onfeedbacksclose'];
+    function UserFeedbacksCtrl($mdDialog, UsersService, user, distance, onfeedbacksclose) {
+        var self = this;
+        self.user = user;
+        self.distance = distance;
+        self.latestFeedbacks = [];
+        self.close = function () {
+            $mdDialog.hide();
+            if (onfeedbacksclose) {
+                onfeedbacksclose();
+            }
+        };
+        UsersService.latestFeedbacks(user).then(function (latestFeedbacks) {
+            self.latestFeedbacks = latestFeedbacks;
+        });
+    }
+
+    /*
      userPhotoRatingsDistance
      */
     userPhotoRatingsDistance.$inject = [];
     function userPhotoRatingsDistance () {
+        var controller = ['$scope', '$mdMedia', '$mdDialog', function ($scope, $mdMedia, $mdDialog) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+            $scope.showUserFeedbacks = function (user, onfeedbacksclose) {
+                $mdDialog.show({
+                    controllerAs: 'ctrl',
+                    controller: 'UserFeedbacksCtrl',
+                    locals: {user: user, distance: $scope.distance, onfeedbacksclose: onfeedbacksclose},
+                    templateUrl: 'app/views/users/user.feedbacks.html',
+                    parent: angular.element(document.body),
+                    clickOutsideToClose: true,
+                    fullscreen: useFullScreen
+                });
+            }
+        }];
         return {
             restrict: 'E',
             scope: {
                 user: '=',
-                distance: '='
+                distance: '=',
+                hidefeedbacks: '=',
+                onfeedbacksclose: '='
             },
+            controller: controller,
             templateUrl: 'app/views/users/directives/user-photo-ratings-distance.html'
         }
-    };
+    }
+
+    /*
+     userLatestFeedbacks
+     */
+    userLatestFeedbacks.$inject = [];
+    function userLatestFeedbacks () {
+        return {
+            restrict: 'E',
+            scope: {
+                user: '=',
+                latestfeedbacks: '='
+            },
+            templateUrl: 'app/views/users/directives/user-latest-feedbacks.html'
+        }
+    }
 
 
 })();
--- web/lsg/server/users/admin.py
+++ web/lsg/server/users/admin.py
@@ -11,5 +11,5 @@ class UserAdmin(admin.ModelAdmin):
 
     def social_links(self, instance):
         return ', '.join(['<a href="%s" target="_blank">%s</a>' % (l, n)
-                          for n, l in instance.social_links])
+                          for n, l, _ in instance.social_links])
     social_links.allow_tags = True
