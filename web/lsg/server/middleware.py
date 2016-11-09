a6925227f014be7942c31a88904eed9e8d1251dc
Rogerio Hilbert Lima <rogerhil@gmail.com>
2016-11-09 22:10:19 +0000

    Privacy policy link and middleare redirect to home.

--- web/lsg/server/middleware.py
+++ web/lsg/server/middleware.py
@@ -23,8 +23,8 @@ class RedirectFallbackMiddleware(object):
             return response
 
         if response.status_code == 404:
-            if request.user.is_authenticated():
-                return http.HttpResponseRedirect('/app/')
+            #if request.user.is_authenticated():
+            #    return http.HttpResponseRedirect('/app/')
             return http.HttpResponseRedirect('/')
 
         return response
