a72f95002928901451e8f3b3c405efc885668bfa
Rogerio Hilbert Lima <rogerhil@gmail.com>
2016-12-12 00:36:45 +0000

    ScheduleEmail send only in that day.

--- web/lsg/server/mail/admin.py
+++ web/lsg/server/mail/admin.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.views.generic import TemplateView
 from django.template.loader import get_template
 from django.template import Context
 
 from request.models import SwapRequest
+from mail.models import EmailSchedule
 
 
 class EmailPreview(TemplateView):
@@ -18,3 +20,21 @@ class EmailPreview(TemplateView):
         context = Context(dict(swap_request=swap_request))
         kwargs['email_rendered'] = template.render(context)
         return super(EmailPreview, self).get_context_data(**kwargs)
+
+
+@admin.register(EmailSchedule)
+class EmailScheduleAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'template_name', 'email', 'send_at', 'sent', 'cancelled',
+                    'swap_request')
+    list_filter = ('sent', 'cancelled')
+    search_fields = ('subject', 'email')
+
+    def get_queryset(self, request):
+        return super(EmailScheduleAdmin, self).get_queryset(request)\
+                                              .select_related('swap_request',
+                                                              'swap_request__requester',
+                                                              'swap_request__requested',
+                                                              'swap_request__requester_game',
+                                                              'swap_request__requested_game',
+                                                          'swap_request__requester_game__platform',
+                                                          'swap_request__requested_game__platform')
--- web/lsg/server/scripts/send_scheduled_emails.py
+++ web/lsg/server/scripts/send_scheduled_emails.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.utils import timezone
 
 from scripts.base import BaseScript
@@ -11,10 +12,13 @@ class SendScheduledEmails(BaseScript):
     name = 'send_scheduled_emails'
 
     def main(self):
+        one_day = timedelta(1)
         now = timezone.now()
+        yesterday = now - one_day  # to send emails in the same day only
+
         deleted = [u.email for u in User.objects.filter(deleted=True)]
-        schedules = EmailSchedule.objects.filter(send_at__lte=now, sent=False,
-                                                 cancelled=False)\
+        schedules = EmailSchedule.objects.filter(send_at__lte=now, send_at_gte=yesterday,
+                                                 sent=False, cancelled=False)\
                                          .exclude(email__in=deleted)
         self.logger.info('Sending %s emails.' % schedules.count())
         for schedule in schedules:
